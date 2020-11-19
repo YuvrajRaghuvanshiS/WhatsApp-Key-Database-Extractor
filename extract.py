040ae1fc696065e5563e659ece96e6ff9c3ff035
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2020-11-19 12:49:11 +0530

    Modiefied banner with try/except

--- extract.py
+++ extract.py
@@ -1,6 +1,10 @@
 from non_essentials.colors import Colors
 
-banner = open('non_essentials\\banner.txt','r')
-banner_content = banner.read()
-print(Colors.OKGREEN + Colors.BOLD + banner_content + Colors.ENDC)
-banner.close()
+try:
+    banner = open('non_essentials\\banner.txt','r')
+    banner_content = banner.read()
+    print(Colors.OKGREEN + Colors.BOLD + banner_content + Colors.ENDC)
+    banner.close()
+    print(Colors.UNDERLINE + Colors.OKGREEN + 'WhatsApp Key / Database Extrator on non-rooted Android' + Colors.ENDC)
+except FileNotFoundError : 
+    print('WhatsApp Key / Database Extrator on non-rooted Android')
