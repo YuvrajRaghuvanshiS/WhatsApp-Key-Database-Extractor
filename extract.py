48e693d9e8c29b2e5c9d2f371d53d538ac4c6305
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2020-11-19 13:49:37 +0530

    New banner and color scheme

--- extract.py
+++ extract.py
@@ -1,10 +1,12 @@
-from non_essentials.colors import Colors
+from termcolor import colored, cprint
 
 try:
     banner = open('non_essentials\\banner.txt','r')
     banner_content = banner.read()
-    print(Colors.OKGREEN + Colors.BOLD + banner_content + Colors.ENDC)
+    cprint(banner_content, 'green', attrs=['bold'])
     banner.close()
-    print(Colors.UNDERLINE + Colors.OKGREEN + 'WhatsApp Key / Database Extrator on non-rooted Android' + Colors.ENDC)
+    cprint('============ WhatsApp Key / Database Extrator on non-rooted Android ============', 'green', attrs=['bold'])
+    cprint('\n================================================================================\n===     This script can extract your WhatsApp msgstore.db (non crypt12,      ===\n===   unencrypted file) and your \'key\' file from \'\\data\\data\\com.whatsapp\'   ===\n===  directory in Android 4.0+ device without root access. However you need  ===\n===   to have JAVA installed on your system in order to \'view the  extract\'. ===\n===  If you don\'t have JAVA installed then you can \'view extract\' later by   ===\n===   running \'view_extract.py\'. The idea is to install a \'Legacy WhatsApp\'  ===\n===       temporarily on your device in order to get the android backup      ===\n===     permission. You WILL NOT lose any data and your current WhatsApp     ===\n===   version will be installed after this device so don\'t panic and don\'t   ===\n=== stop this script while it\'s working. However if something fails you can  ===\n===    run \'restore_whatsapp.py\' so reinstall current WhatsApp or simply     ===\n=== update that from Google Play Store. But it\'s always a good idea to take  ===\n===  a backup. For that go to \'WhatsApp settings\\Chat Settings\\Chat Backup\'  ===\n===              here take a local bacakup. Prepare for Worst.               ===\n===                      Script by : Yuvraj Raghuvanshi                      ===\n===                      Github.com/YuvrajRaghuvanshiS                       ===\n================================================================================', 'green')
 except FileNotFoundError : 
     print('WhatsApp Key / Database Extrator on non-rooted Android')
+
