266ec517260ab3e8fbd8dbd23417a7aa7f1a2adc
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2020-11-20 16:44:36 +0530

    winUSB used adb var, device name fix

--- extract.py
+++ extract.py
@@ -138,12 +138,12 @@ def WindowsTCP(deviceIP, devicePort) :
     AfterConnect()
 
 def WindowsUSB() : 
-    os.system("bin\\adb.exe kill-server")
+    os.system(adb + ' kill-server')
     CustomPrint('Plug device via USB now..', 'green')
-    os.system('bin\\adb.exe start-server')
-    os.system('bin\\adb.exe wait-for-device')
-    deviceName='bin\\adb.exe shell getprop ro.product.model'
-    CustomPrint('Connected to ' + str(subprocess.Popen(deviceName.split(), stdout=subprocess.PIPE).communicate()[0]) , 'green')
+    os.system(adb + ' start-server')
+    os.system(adb + ' wait-for-device')
+    deviceName= adb + ' shell getprop ro.product.model'
+    CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\r)", str(check_output(deviceName))).group(1) , 'green')
     AfterConnect()
 
 if __name__ == "__main__":
