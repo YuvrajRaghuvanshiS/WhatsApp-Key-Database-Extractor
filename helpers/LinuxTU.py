a158ce01fe17d9efb58e6ee04b1be42f80689930
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2020-11-22 00:57:47 +0530

    linux custom AfterConnect

--- helpers/LinuxTU.py
+++ helpers/LinuxTU.py
@@ -4,9 +4,22 @@ import os
 from subprocess import check_output
 
 # Global variables
-delete = 'rm -rf'
-confirmDelete = ''
 helpers = 'helpers/'
+
+def AfterConnect() : 
+    SDKVersion = int(re.search('[0-9]{2,3}', str(check_output('adb shell getprop ro.build.version.sdk'))).group(0))
+    if (SDKVersion <= 13) : 
+        CustomPrint('Unsupported device. This method only works on Android v4.0 or higer.', 'green')
+        CustomPrint('Cleaning up temporary direcory.', 'green')
+        os.system('rm -rf tmp/*')
+        Exit()
+    WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output('adb shell pm path com.whatsapp'))).group(1) + 'apk'
+    if not (WhatsAppapkPath) : CustomPrint('Looks like WhatsApp is not installed on device.') ; Exit()
+    SDPath = re.search("(?<=b')(.*)(?=\\\\r)", str(check_output('adb shell "echo $EXTERNAL_STORAGE"'))).group(1)
+    contentLength = int(re.search("(?<=Content-Length:)(.*[0-9])(?=)", str(check_output('curl -sI http://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'))).group(1))
+    versionName = re.search("(?<=versionName=)(.*?)(?=\\\\r)", str(check_output('adb shell dumpsys package com.whatsapp'))).group(1)
+    CustomPrint('WhatsApp V' + versionName + ' installed on device')
+    return 1
 
 def Exit():
     CustomPrint('\nExiting...', 'green')
@@ -24,7 +37,7 @@ def LinuxTCP(deviceIP, devicePort) :
         Exit()
     deviceName= 'adb shell getprop ro.product.model'
     CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(deviceName.split()))).group(1) , 'green')
-    return 1
+    AfterConnect()
 
 def LinuxUSB() : 
     try : 
@@ -37,4 +50,4 @@ def LinuxUSB() :
         Exit()
     deviceName= 'adb shell getprop ro.product.model'
     CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(deviceName.split()))).group(1) , 'green')
-    return 1
+    AfterConnect()
