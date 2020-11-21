c21b5db6c44da7664b8a1a461581d358a52b287b
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2020-11-22 02:40:06 +0530

    some split() SDPath not working

--- helpers/LinuxTU.py
+++ helpers/LinuxTU.py
@@ -11,17 +11,17 @@ appURLWhatsAppCDN = 'https://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'
 appURLWhatsCryptCDN = 'https://whatcrypt.com/WhatsApp-2.11.431.apk'
 
 def AfterConnect() : 
-    SDKVersion = int(re.search('[0-9]{2,3}', str(check_output('adb shell getprop ro.build.version.sdk'))).group(0))
+    SDKVersion = int(re.search('[0-9]{2,3}', str(check_output('adb shell getprop ro.build.version.sdk'.split()))).group(0))
     if (SDKVersion <= 13) : 
         CustomPrint('Unsupported device. This method only works on Android v4.0 or higer.', 'green')
         CustomPrint('Cleaning up temporary direcory.', 'green')
         os.system('rm -rf tmp/*')
         Exit()
-    WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output('adb shell pm path com.whatsapp'))).group(1) + 'apk'
+    WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output('adb shell pm path com.whatsapp'.split()))).group(1) + 'apk'
     if not (WhatsAppapkPath) : CustomPrint('Looks like WhatsApp is not installed on device.') ; Exit()
-    SDPath = re.search("(?<=b')(.*)(?=\\\\r)", str(check_output('adb shell "echo $EXTERNAL_STORAGE"'))).group(1)
-    contentLength = int(re.search("(?<=Content-Length:)(.*[0-9])(?=)", str(check_output('curl -sI http://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'))).group(1))
-    versionName = re.search("(?<=versionName=)(.*?)(?=\\\\r)", str(check_output('adb shell dumpsys package com.whatsapp'))).group(1)
+    SDPath = re.search("(?<=b')(.*)(?=\\\\r)", str(check_output('adb shell "echo $EXTERNAL_STORAGE"'.split()))).group(1)
+    contentLength = int(re.search("(?<=Content-Length:)(.*[0-9])(?=)", str(check_output('curl -sI http://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'.split()))).group(1))
+    versionName = re.search("(?<=versionName=)(.*?)(?=\\\\r)", str(check_output('adb shell dumpsys package com.whatsapp'.split()))).group(1)
     CustomPrint('WhatsApp V' + versionName + ' installed on device')
     downloadAppFrom = appURLWhatsAppCDN if(contentLength == 18329558) else appURLWhatsCryptCDN
     if (version.parse(versionName) > version.parse('2.11.431')) :
@@ -53,7 +53,7 @@ def LinuxBashDependencies():
     if(error!=None) : 
         CustomPrint(error,'red')
         Exit()
-    CustomPrint(output, 'green')
+    CustomPrint(re.search("(?<=b')(.*)(?=\\\\n)", str(output)), 'green'
 
 def LinuxTCP(deviceIP, devicePort) : 
     LinuxBashDependencies()
