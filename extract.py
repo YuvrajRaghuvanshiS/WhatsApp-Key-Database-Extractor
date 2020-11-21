53c9253938e48692c68fe6083f247b8241c1e855
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2020-11-21 12:37:22 +0530

    Banner not found exception

--- extract.py
+++ extract.py
@@ -78,7 +78,7 @@ def BackupWhatsApp(SDKVersion, versionName):
 
 def CheckBinIfWindows() : 
     if (isWindows and not os.path.isdir('bin')) : 
-        CustomPrint('I can not find /bin folder, check again...', 'green')
+        CustomPrint('I can not find bin folder, check again...', 'green')
         Exit()
     pass
 
@@ -157,10 +157,13 @@ def RemoveWhatsApp(SDKVersion):
 
 def ShowBanner() : 
     banner_path = 'non_essentials/banner.txt'
-    banner = open(banner_path,'r')
-    banner_content = banner.read()
-    CustomPrint(banner_content, 'green', ['bold'])
-    banner.close()
+    try : 
+        banner = open(banner_path,'r')
+        banner_content = banner.read()
+        CustomPrint(banner_content, 'green', ['bold'])
+        banner.close()
+    except Exception as e : 
+        CustomPrint(e)
     CustomPrint('============ WhatsApp Key / Database Extrator on non-rooted Android ============', 'green', ['bold'])
     CustomPrint('\n================================================================================\n===     This script can extract your WhatsApp msgstore.db (non crypt12,      ===\n===   unencrypted file) and your \'key\' file from \'\\data\\data\\com.whatsapp\'   ===\n===  directory in Android 4.0+ device without root access. However you need  ===\n===   to have JAVA installed on your system in order to \'view the  extract\'. ===\n===  If you don\'t have JAVA installed then you can \'view extract\' later by   ===\n===   running \'view_extract.py\'. The idea is to install a \'Legacy WhatsApp\'  ===\n===       temporarily on your device in order to get the android backup      ===\n===     permission. You WILL NOT lose any data and your current WhatsApp     ===\n===   version will be installed after this process so don\'t panic and don\'t  ===\n=== stop this script while it\'s working. However if something fails you can  ===\n===    run \'restore_whatsapp.py\' and reinstall current WhatsApp or simply    ===\n=== update that from Google Play Store. But it\'s always a good idea to take  ===\n===  a backup. For that go to \'WhatsApp settings\\Chat Settings\\Chat Backup\'  ===\n===              here take a local bacakup. Prepare for Worst.               ===\n===                                                                          ===\n===                      Script by : Yuvraj Raghuvanshi                      ===\n===                      Github.com/YuvrajRaghuvanshiS                       ===\n================================================================================', 'green')
 
