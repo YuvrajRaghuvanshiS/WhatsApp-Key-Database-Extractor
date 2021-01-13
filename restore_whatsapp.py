38287faba7b419918164083ad8f4cfcb290f6da2
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-13 16:17:10 +0530

    Restore whatsapp

---
+++ restore_whatsapp.py
@@ -0,0 +1,30 @@
+from helpers.CustomCI import CustomPrint
+import platform
+import os
+
+# Detect OS
+isWindows = False
+isLinux = False
+if platform.system() == 'Windows' : isWindows = True 
+if platform.system() == 'Linux' : isLinux = True
+
+# Global command line helpers
+adb = 'bin\\adb.exe'
+tmp = 'tmp\\'
+if(isLinux) : 
+    adb = 'adb'
+    tmp = 'tmp/'
+
+def ReinstallWhatsApp():
+    CustomPrint('Reinstallting original WhatsApp.')
+    if(os.path.isfile(tmp + 'WhatsAppbackup.apk')) :
+        try : 
+            os.system(adb + ' install -r -d ' + tmp + 'WhatsAppbackup.apk')
+        except Exception as e : 
+            print(e)
+            CustomPrint('Could not restore WhatsApp, install from Play Store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.')
+    else : 
+        CustomPrint('Could not find backup APK, install from play store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.','red')
+
+
+ReinstallWhatsApp()
