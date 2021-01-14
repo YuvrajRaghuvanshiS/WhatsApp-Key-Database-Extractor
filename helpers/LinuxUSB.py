ea62b9bb171ca18e773d16f53b37b5a11eb0247b
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-14 21:54:39 +0530

    test complete, remove try, view extract not working

--- helpers/LinuxUSB.py
+++ helpers/LinuxUSB.py
@@ -1,3 +1,4 @@
+from helpers.ADBDeviceSerialId import GetASBDeviceSerialId
 import re
 from CustomCI import CustomInput, CustomPrint
 import os
@@ -7,21 +8,25 @@ import wget
 import subprocess
 
 # Global variables
+serialId = str(GetASBDeviceSerialId())
 appURLWhatsAppCDN = 'https://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'
 appURLWhatsCryptCDN = 'https://whatcrypt.com/WhatsApp-2.11.431.apk'
 
 def AfterConnect() : 
-    SDKVersion = int(re.search('[0-9]{2,3}', str(check_output('adb shell getprop ro.build.version.sdk'.split()))).group(0))
+    _sdkVersionText = 'adb -s ' + serialId + ' shell getprop ro.build.version.sdk'
+    SDKVersion = int(re.search('[0-9]{2,3}', str(check_output(_sdkVersionText.split()))).group(0))
     if (SDKVersion <= 13) : 
         CustomPrint('Unsupported device. This method only works on Android v4.0 or higer.', 'green')
         CustomPrint('Cleaning up temporary direcory.', 'green')
         os.system('rm -rf tmp/*')
         Exit()
-    WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output('adb shell pm path com.whatsapp'.split()))).group(1) + 'apk'
+    _waPathText = 'adb -s ' + serialId + ' shell pm path com.whatsapp'
+    WhatsAppapkPath = re.search('(?<=package:)(.*)(?=apk)', str(check_output(_waPathText.split()))).group(1) + 'apk'
     if not (WhatsAppapkPath) : CustomPrint('Looks like WhatsApp is not installed on device.') ; Exit()
     #SDPath = re.search("(?<=b')(.*)(?=\\\\n)", str(check_output('adb shell "echo $EXTERNAL_STORAGE"'.split()))).group(1)
     contentLength = int(re.search("(?<=Content-Length:)(.*[0-9])(?=)", str(check_output('curl -sI http://www.cdn.whatsapp.net/android/2.11.431/WhatsApp.apk'.split()))).group(1))
-    versionName = re.search("(?<=versionName=)(.*?)(?=\\\\n)", str(check_output('adb shell dumpsys package com.whatsapp'.split()))).group(1)
+    _versionNameText = 'adb -s ' + serialId + ' shell dumpsys package com.whatsapp'
+    versionName = re.search("(?<=versionName=)(.*?)(?=\\\\n)", str(check_output(_versionNameText.split()))).group(1)
     CustomPrint('WhatsApp V' + versionName + ' installed on device')
     downloadAppFrom = appURLWhatsAppCDN if(contentLength == 18329558) else appURLWhatsCryptCDN
     if (version.parse(versionName) > version.parse('2.11.431')) :
@@ -35,7 +40,7 @@ def AfterConnect() :
 
 def Exit():
     CustomPrint('\nExiting...', 'green')
-    os.system('bin\\adb.exe kill-server')
+    os.system('adb kill-server')
     quit()
 
 def LinuxBashDependencies():
@@ -56,14 +61,6 @@ def LinuxBashDependencies():
 
 def LinuxUSB() : 
     LinuxBashDependencies()
-    try : 
-        os.system('adb kill-server')
-        os.system('adb start-server')
-        CustomPrint('Plug device via USB now..', 'green')
-        os.system('adb wait-for-device')
-    except Exception as e : 
-        CustomPrint(e)
-        Exit()
-    deviceName= 'adb shell getprop ro.product.model'
-    CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(deviceName.split()))).group(1) , 'green')
+    _deviceName= 'adb -s ' + serialId + ' shell getprop ro.product.model'
+    CustomPrint('Connected to ' + re.search("(?<=b')(.*)(?=\\\\n)", str(check_output(_deviceName.split()))).group(1) , 'green')
     return AfterConnect()
