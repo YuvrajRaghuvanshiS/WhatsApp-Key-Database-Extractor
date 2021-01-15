e8de06439a16330a9767b6329d0a9e476d951012
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-15 23:19:35 +0530

    modified depencies script

--- bin/termux_dependencies.sh
+++ bin/termux_dependencies.sh
@@ -1,8 +1,3 @@
-run_install()
-{
-    apt update && apt install curl grep tar proot wget -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
-}
-
 echo "Installing dependencies..."
-run_install
+apt update && apt install curl grep tar proot wget -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
 echo "Dependencies installed successfully. Starting..."
