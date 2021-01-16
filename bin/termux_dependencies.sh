8d3e87c8a2e8782ce8a7e909f4b4f39438c56921
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-16 17:02:55 +0530

    termux dependencies modfied with conflicts.

--- bin/termux_dependencies.sh
+++ bin/termux_dependencies.sh
@@ -1,3 +1,14 @@
 echo "Installing dependencies..."
-apt update && apt install curl grep tar proot wget -y && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
-echo "Dependencies installed successfully. Starting..."
+echo "Updating Termux"
+apt update
+echo "Allow storage permission for storing extraced whatsapp.ab in interal storage."
+termux-setup-storage 
+echo "Done. Installing various required components."
+apt install curl grep fish tar proot wget -y
+echo "Done. Installing ADB for Termux"
+wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
+echo "Done. Installing Java for termux."
+wget https://raw.githubusercontent.com/Hax4us/java/master/installjava && sh installjava
+echo "Done installing all dependencies. Now running proot."
+fish
+proot login
