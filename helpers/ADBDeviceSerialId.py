1bba28f2489e89eb5d7aa34928636e9005a02b03
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-18 22:30:54 +0530

    Fixed : LinuxUSB asking for device id twice. Problems in LinuxDependencies.

--- helpers/ADBDeviceSerialId.py
+++ helpers/ADBDeviceSerialId.py
@@ -2,23 +2,23 @@ from CustomCI import CustomInput, CustomPrint
 import os
 import platform
 
-# Detect OS
-isWindows = False
-isLinux = False
-if platform.system() == 'Windows' : isWindows = True 
-if platform.system() == 'Linux' : isLinux = True
+def init() : 
+    # Detect OS
+    isWindows = False
+    isLinux = False
+    if platform.system() == 'Windows' : isWindows = True 
+    if platform.system() == 'Linux' : isLinux = True
 
-# Global command line helpers
-currDir = os.path.dirname(os.path.realpath(__file__))
-rootDir = os.path.abspath(os.path.join(currDir, '..'))
+    # Global command line helpers
+    currDir = os.path.dirname(os.path.realpath(__file__))
+    rootDir = os.path.abspath(os.path.join(currDir, '..'))
 
-adb = rootDir + '\\bin\\adb.exe'
-tmp = rootDir + '\\tmp\\'
-if(isLinux) : 
-    adb = 'adb'
-    tmp = 'tmp/'
+    adb = rootDir + '\\bin\\adb.exe'
+    tmp = rootDir + '\\tmp\\'
+    if(isLinux) : 
+        adb = 'adb'
+        tmp = 'tmp/'
 
-os.system(adb + ' devices')
-ADBSerialId = CustomInput('Choose device from "List of devices attached"\nFor example : 7835fd84543/emulator-5554 : ', 'green')
-def GetASBDeviceSerialId() : 
+    os.system(adb + ' devices')
+    ADBSerialId = CustomInput('Choose device from "List of devices attached"\nFor example : 7835fd84543/emulator-5554 : ', 'green')
     return ADBSerialId
