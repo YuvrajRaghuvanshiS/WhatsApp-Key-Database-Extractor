747d6c0fb2c959b730aaadb8b0ad26a7dd3ea7b8
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-18 21:57:11 +0530

    Restoration works with ADBSerialId Not fully tested but I do believe in what I code.

--- restore_whatsapp.py
+++ restore_whatsapp.py
@@ -1,5 +1,5 @@
 from helpers.CustomCI import CustomPrint
-from helpers.ADBDeviceSerialId import GetASBDeviceSerialId
+import helpers.ADBDeviceSerialId as deviceId
 import platform
 import os
 
@@ -10,22 +10,23 @@ if platform.system() == 'Windows' : isWindows = True
 if platform.system() == 'Linux' : isLinux = True
 
 # Global command line helpers
-adb = 'bin\\adb.exe -s ' + str(GetASBDeviceSerialId())
+adb = 'bin\\adb.exe -s '
 tmp = 'tmp\\'
 if(isLinux) : 
-    adb = 'adb -s ' + str(GetASBDeviceSerialId())
+    adb = 'adb -s '
     tmp = 'tmp/'
 
-def ReinstallWhatsApp():
+def ReinstallWhatsApp(ADBSerialId):
     CustomPrint('Reinstallting original WhatsApp.')
     if(os.path.isfile(tmp + 'WhatsAppbackup.apk')) :
         try : 
-            os.system(adb + ' install -r -d ' + tmp + 'WhatsAppbackup.apk')
+            os.system(adb + ADBSerialId + ' install -r -d ' + tmp + 'WhatsAppbackup.apk')
         except Exception as e : 
             print(e)
             CustomPrint('Could not restore WhatsApp, install from Play Store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.')
     else : 
         CustomPrint('Could not find backup APK, install from play store.\nHowever if it crashes then you have to clear storage/clear data from settings => app settings => WhatsApp.','red')
 
-
-ReinstallWhatsApp()
+if __name__ == "__main__":
+    ADBSerialId = deviceId.init()
+    ReinstallWhatsApp(ADBSerialId)
