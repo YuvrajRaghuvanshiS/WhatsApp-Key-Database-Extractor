24d882d3a3b5431b6bb48b39eae8f7edac88859d
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-01-18 20:23:42 +0530

    CustomCI for windows also.

--- helpers/CustomCI.py
+++ helpers/CustomCI.py
@@ -1,20 +1,8 @@
 from termcolor import colored, cprint
 import platform
 
-# Detect OS
-isWindows = False
-isLinux = False
-if platform.system() == 'Windows' : isWindows = True 
-if platform.system() == 'Linux' : isLinux = True
-
 def CustomInput(textToInput, color = 'green', attr=[]) : 
-    if(isWindows) : 
-        return input(textToInput).casefold()
-    else : 
-        return input(colored(textToInput, color, attrs=attr)).casefold()
+    return input(colored(textToInput, color, attrs=attr)).casefold()
 
 def CustomPrint(textToPrint, color = 'green', attr=[]) : 
-    if(isWindows) : 
-        print(textToPrint)
-    else : 
-        cprint(textToPrint, color, attrs=attr)
+    cprint(textToPrint, color, attrs=attr)
