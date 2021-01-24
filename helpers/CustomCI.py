d834dc8967352131a1a7115e0b05bda19618d8d0
Vrashank Vats <VrashankVats12@gmail.com>
2021-01-24 20:53:16 +0530

    Removed casefold Fixed : ADB device not found when all letter changed to small.

--- helpers/CustomCI.py
+++ helpers/CustomCI.py
@@ -1,7 +1,7 @@
 from termcolor import colored, cprint
 
 def CustomInput(textToInput, color = 'green', attr=[]) : 
-    return input(colored(textToInput, color, attrs=attr)).casefold()
+    return input(colored(textToInput, color, attrs=attr))
 
 def CustomPrint(textToPrint, color = 'green', attr=[]) : 
     cprint(textToPrint, color, attrs=attr)
