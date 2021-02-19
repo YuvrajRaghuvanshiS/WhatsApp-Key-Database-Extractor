694970510f2a109277e129ddb01286f48f3df415
Robert S <31773450+roberts01@users.noreply.github.com>
2021-02-19 18:35:12 +0100

    Corrected Termcolor

--- helpers/termux_dependencies.py
+++ helpers/termux_dependencies.py
@@ -4,11 +4,12 @@ import os
 def InstallTermuxDependencies() : 
     CustomPrint("Installing dependencies...")
     CustomPrint("Updating Termux")
-    os.system('pkg update')
+    os.system('pkg update && pkg upgrade')
     CustomPrint("Allow storage permission for storing extracted whatsapp.ab in interal storage:")
     os.system('termux-setup-storage')
     CustomPrint("Done. Installing required dependencies...")
-    os.system('pkg install curl grep tar proot wget termcolor -y')
+    os.system('pkg install curl grep tar proot wget -y')
+    os.system('pip install termcolor')
     os.system('wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh')
     try : 
         os.system('rm -r -f installjava') # Deleting any previous instance of installjava.
