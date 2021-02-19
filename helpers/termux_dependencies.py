a4890aee074a359de02812e6f42f15ba10d91dea
Robert S <31773450+roberts01@users.noreply.github.com>
2021-02-19 17:00:47 +0100

    Updated termux requirements
    Also maybe move this to the root of the Termux folder

--- helpers/termux_dependencies.py
+++ helpers/termux_dependencies.py
@@ -4,19 +4,21 @@ import os
 def InstallTermuxDependencies() : 
     CustomPrint("Installing dependencies...")
     CustomPrint("Updating Termux")
-    os.system('apt update')
-    CustomPrint("Allow storage permission for storing extraced whatsapp.ab in interal storage.")
+    os.system('pkg update')
+    ustomPrint("Allow storage permission for storing extracted whatsapp.ab in interal storage:")
     os.system('termux-setup-storage')
-    CustomPrint("Done. Installing various required components.")
-    os.system('apt install curl grep tar proot wget -y')
-    CustomPrint("Done. Installing ADB for Termux")
+    CustomPrint("Done. Installing required dependencies...")
+    os.system('pkg install curl grep tar proot wget termcolor -y')
     os.system('wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh')
-    CustomPrint("Done. Installing Java for termux.")
     try : 
-        os.system('rm -rf installjava') # Deleting any previous instance of installjava.
+        os.system('rm -r -f installjava') # Deleting any previous instance of installjava.
     except Exception as e : 
         pass
-    os.system('wget https://raw.githubusercontent.com/Hax4us/java/master/installjava && sh installjava')
-    CustomPrint("Done installing all dependencies. Now running proot.")
+    os.system('wget https://raw.githubusercontent.com/MasterDevX/java/master/installjava && sh installjava')
+    os.system('proot login')
+    CustomPrint("Connecting ADB with local device:")
+    os.system('adb connect localhost')
+    os.system('cd WhatsApp-Key-Databse-Extractor/')
+    CustomPrint("Succesfully installed all dependencies.")
 
 InstallTermuxDependencies()
