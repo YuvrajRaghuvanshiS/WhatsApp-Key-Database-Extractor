b73aea7f327fcc3c35e0da654b147913b94b6cae
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-02-22 11:00:00 +0530

    #35 Added brew in linux_dependencies.sh

--- bin/linux_dependencies.sh
+++ bin/linux_dependencies.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 echo "Installing dependencies... Hold on."
+echo "Which platform are you on?"
+echo "L : Linux (Kali, Ubuntu, ParrotSec)"
+echo "M : Mac"
+read platform
+echo $platform
+
 echo "Updating system."
 
-apt-get update
+if [ $platform = 'l' ]; then
+	pkg_man=apt-get
+else
+	pkg_man=brew
+fi
+
+$pkg_man update
 
 echo "Installing packages."
 
 for package in adb curl grep tar openjdk-11-jdk p7zip-full
 do
 
-apt-get install -y $package
+$pkg_man install -y $package
 
 done
 
