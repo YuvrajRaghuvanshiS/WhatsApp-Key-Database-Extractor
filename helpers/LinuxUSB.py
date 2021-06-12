c05e66657dd562c3248f071d8a0633c6fe52ead8
Yuvraj Raghuvanshi <YuvrajRaghuvanshi.S@protonmail.com>
2021-06-12 20:12:06 +0530

    Attempt to fix "stuck on downloading legacy"

--- helpers/LinuxUSB.py
+++ helpers/LinuxUSB.py
@@ -13,7 +13,7 @@ except ImportError:
     except:
         os.system('python3 -m pip install packaging requests tqdm')
 
-from CustomCI import CustomPrint
+from CustomCI import CustomPrint, CustomInput
 
 # Global variables
 appURLWhatsAppCDN = 'https://web.archive.org/web/20141111030303if_/http://www.whatsapp.com/android/current/WhatsApp.apk'
@@ -73,8 +73,23 @@ def AfterConnect(ADBSerialId):
 def DownloadApk(url, fileName):
     # Streaming, so we can iterate over the response.
     response = requests.get(url, stream=True)
-    totalSizeInBytes = int(response.headers.get(
-        'x-archive-orig-content-length')) or int(response.headers.get('content-length', 0))
+    totalSizeInBytes = response.headers.get(
+        'x-archive-orig-content-length') or response.headers.get('content-length', 0)
+    if(totalSizeInBytes):
+        # Fixed where it stuck on "Downloading legacy WhatsApp V2.11.431 to helpers folder"
+        totalSizeInBytes = int(totalSizeInBytes)
+    else:
+        # totalSizeInBytes must be null
+        CustomPrint('\aFor some reason I could not download Legacy WhatsApp, you need to download it on your own now from either of the links given below : ', 'red')
+        print('\n')
+        CustomPrint('1. \'' + appURLWhatsAppCDN +
+                    '\' (official\'s archive)', 'red')
+        CustomPrint('2. \'' + appURLWhatsCryptCDN +
+                    '\' unofficial website.', 'red')
+        print('\n')
+        CustomPrint(
+            'Once downloaded rename it to \'LegacyWhatsApp.apk\' exactly and put in \'helpers\' folder.', 'red')
+        Exit()
     blockSize = 1024  # 1 Kibibyte
     progressBar = tqdm(total=totalSizeInBytes, unit='iB', unit_scale=True)
     with open('helpers/temp.apk', 'wb') as file:
