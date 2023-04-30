f672f9993cfa100243accec1cc2a558feab18b96
Yuvraj Raghuvanshi <yuvraj@inkredo.in>
2023-05-01 00:02:34 +0530

    Formatted using black

--- helpers/custom_ci.py
+++ helpers/custom_ci.py
@@ -8,37 +8,45 @@ try:
     from termcolor import colored, cprint
 except ImportError:
     try:
-        os.system('pip3 install termcolor')
+        os.system("pip3 install termcolor")
     except Exception:
-        os.system('python3 -m pip install termcolor')
+        os.system("python3 -m pip install termcolor")
 
-if not (os.path.isdir('log')):
-    os.mkdir('log')
-logging.basicConfig(filename='log/wa_kdbe.log', level=logging.DEBUG, format='')
+if not (os.path.isdir("log")):
+    os.mkdir("log")
+logging.basicConfig(filename="log/wa_kdbe.log", level=logging.DEBUG, format="")
 masked = []
 
 
-def custom_input(text_to_input, color='green', attr=[], is_get_time=True, is_log=True):
-    time = get_time() if is_get_time else ''
-    data = input(colored(f'{time}{text_to_input}', color, attrs=attr))
-    if(is_log):
-        logging.debug(f'{time}{text_to_input}{data}')
+def custom_input(text_to_input, color="green", attr=[], is_get_time=True, is_log=True):
+    time = get_time() if is_get_time else ""
+    data = input(colored(f"{time}{text_to_input}", color, attrs=attr))
+    if is_log:
+        logging.debug(f"{time}{text_to_input}{data}")
     else:
-        logging.debug(f'{time}{text_to_input}********')
+        logging.debug(f"{time}{text_to_input}********")
         # Add that password in list, and mask that while printing also.
         masked.append(data)
     return data
 
 
-def custom_print(text_to_print, color='green', attr=[], is_get_time=True, is_log=True, is_print=True, end='\n'):
-    time = get_time() if is_get_time else ''
+def custom_print(
+    text_to_print,
+    color="green",
+    attr=[],
+    is_get_time=True,
+    is_log=True,
+    is_print=True,
+    end="\n",
+):
+    time = get_time() if is_get_time else ""
     text_to_print = str(text_to_print)
-    if(is_print):
-        cprint(f'{time}{text_to_print}', color, attrs=attr, end=end)
+    if is_print:
+        cprint(f"{time}{text_to_print}", color, attrs=attr, end=end)
     else:
         pass
-    if(is_log):
-        logging.debug(f'{time}{text_to_print}')
+    if is_log:
+        logging.debug(f"{time}{text_to_print}")
     else:
         # Search for password and mask.
         for i in masked:
